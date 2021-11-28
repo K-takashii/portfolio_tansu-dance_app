9217b9e18c1fcfd3c925e343e3e700463c3d33f0
“K-takashii” <nanase.sigre.1627.yorushika@gmail.com>
2021-11-28 12:23:01 +0000

    Fix image

--- db/migrate/20211116124943_rename_micropost.rb
+++ db/migrate/20211116124943_rename_micropost.rb
@@ -1,9 +1,9 @@
 class RenameMicropost < ActiveRecord::Migration[5.2]
   def change
-    remove_index :tag_maps, column: [:micropost_id, :tag_id]
-    remove_reference :tag_maps, :micropost
-    
-    add_reference :tag_maps, :clothe
-    add_index :tag_maps, [:clothe_id,:tag_id],unique: true
+    #remove_index :tag_maps, column: [:micropost_id, :tag_id]
+    #remove_reference :tag_maps, :micropost
+
+    #add_reference :tag_maps, :clothe
+    #add_index :tag_maps, [:clothe_id,:tag_id],unique: true
   end
 end
