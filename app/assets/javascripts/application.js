46029fc451b610288e547896ad91d20fc7d376c2
“K-takashii” <nanase.sigre.1627.yorushika@gmail.com>
2021-12-04 15:32:24 +0000

    Fix .js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,57 +35,17 @@ $(window).on('scroll', function() {
   if ( (scrollHeight - scrollPosition) / scrollHeight <= 0.05) {
     progress = true
     var nextLink = $('span.next > a')[0]
-    console.log($(nextLink).length);
     if ( $(nextLink).length == 0 ){
       return;
     }
-    console.log(parseInt($('span.current').html()));
     var nextPage =  parseInt($('span.current').html())+1;
     $.ajax({
       type: 'GET',
       url: '/categories/more?page=' + nextPage,
       dataType: 'script'
     }).done(function (res) {
-      //console.log(res)
     }).always(function () {
       progress = false
     })
-    // console.log($('.jscroll').find('span.next a'));
-    // $('.jscroll').jscroll({
-    //   contentSelector: '.jscroll',
-    //   nextSelector: 'span.next:last a'
-    // });
   }
 });
-
-// $(document).ready(function () {
-//   $('.jscroll').find('span.next').find('a').on('click', function (e) {
-//     // location.href = e.target.href
-//   });
-// });
-
-/*
-$(window).on('scroll', function() {
-  scrollHeight = $(document).height();
-  scrollPosition = $(window).height() + $(window).scrollTop();
-  if ( (scrollHeight - scrollPosition) / scrollHeight <= 0.05) {
-    $('.jscroll').jscroll({
-      autoTrigger: true,
-      debug: true,
-      contentSelector: '.scroll-list',
-      nextSelector: 'span.next:last a'
-    });
-  }
-});
-*/
-
-
-/*
-$(document).ready(function(){
-  $('.jscroll').jscroll({
-    autoTrigger: true,
-      contentSelector: '.scroll-list',
-      nextSelector: 'span.next a'
-    });
-})
-*/
